582a23d974fc3ed6eb1cf24f621114dffc1f16d9
bosarn <58468105+bosarn@users.noreply.github.com>
2020-03-21 14:33:19 +0100

    Tutorial complete

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MessageService } from '../messages.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
@@ -10,7 +10,7 @@ export class MessagesComponent implements OnInit {
 
   constructor(public messageService: MessageService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
